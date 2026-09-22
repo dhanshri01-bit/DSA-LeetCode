98ad95f5cc0af4a25dd46fa757fe9bc8a1ad8606
Dhanshri Wagh <dhanshri2828@gmail.com>
2026-09-22 14:21:19 +0530

    Time: 79 ms (5.09%) | Memory: 105.2 MB (15.27%) - LeetSync

---
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+
+        int i, j, max, n, balanced, ans, remove,low,high,mid;
+        n = nums.size();
+
+        sort(nums.begin(),nums.end());
+        ans = INT_MAX;
+        for(i = 0; i < n; i++)
+        {
+            low = 0;
+            high = n - 1;
+            j = i;
+           
+
+            while(low <= high)
+            {
+                 mid = (low + high) / 2;
+                if(nums[mid] <= (long long)nums[i] * k)
+                {
+                    j = mid;
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+
+            remove = n - ( j - i + 1);
+
+            if(ans > remove)
+            {
+                ans = remove;
+            }
+        }
+
+        cout<<n<<endl;
+
+
+        return ans;
+    }
+};                                                                                                                                                                                                                            
